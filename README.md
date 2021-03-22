f142a9c7d3fbf58faa172e97382bb5d230176e51
Becca Wyland <rwyland@uwalumni.com>
2021-03-21 22:12:55 -0500

    create readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # password-generator
+
+## Purpose
+Create a password generator that's output will be based on selected password criteria.
+
+## Built With
+* HTML
+* CSS
+* Javascript
+
+## Website
+https://rwyland189.github.io/password-generator/
+
+## Screenshot
+![Password](password-screenshot.png)
+
+## Contribution
+Made by Becca Wyland
--- script.js
+++ script.js
@@ -9,11 +9,10 @@ function generatePassword(lower, upper, number, symbol) {
 
   // make sure password length fits criteria
   if (passwordLength >= 8 && passwordLength <= 128) {
-    // execute password generation?
   } 
   else {
     alert('Password must be between 8 and 128 characters.');
-    return; // still not returning the prompt?
+    return; // not returning the prompt?
   }
 
   // store user input for yes/no uppercase characters
@@ -28,7 +27,7 @@ function generatePassword(lower, upper, number, symbol) {
   // store user input for yes/no special characters
   var passwordSymbol = confirm('Include special characters?');
 
-  // this object contains all the password criteria functions below. need to incorporate this into generatePassword
+  // this object contains all the password criteria functions below
   var random = {
     lower: getLowercase,
     upper: getUppercase,
@@ -65,17 +64,17 @@ function generatePassword(lower, upper, number, symbol) {
   var typesCount = passwordLower + passwordUpper + passwordNumber + passwordSymbol;
   console.log('typesCount: ', typesCount);
 
+  // if no criteria are selected alert the user and prompt them to select criteria again
+  if(typesCount === 0) {
+    alert('Must select at least one criteria.');
+    return;
+  }
+
   // filter out unselected password criteria by looping through each item using Object.values
   var typesArr = [{passwordUpper}, {passwordLower}, {passwordNumber}, {passwordSymbol}].filter(
     item => Object.values(item)[0]
   );
   console.log('typesArr: ', typesArr);
-
-  // if no criteria are selected alert the user and prompt them to select criteria again
-  if(typesCount === 0) {
-    alert('Must select at least one criteria.');
-    //how to redo password prompts then?
-  }
 
   // generate various characters based on which criteria were selected
   // Loop over the length and call the approrpiate generator functions
@@ -87,13 +86,13 @@ function generatePassword(lower, upper, number, symbol) {
       // console not showing anything
       console.log('funcName: ', funcName);
 
-      generatedPassword === random[funcName]();
+      generatedPassword += random[funcName]();
       // console not showing anything
       console.log(generatedPassword);
     });
   }
     
-}
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -104,26 +103,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //The task it will perform the function 
-
-
-
-// my own comments for understanding
-// (1) the variable titled generateBtn's value is equal to a task that locates the id #generate in the html (<button>) by using the querySelector
-
-// (2) now that we've found the <button> we want, whenever it gets clicked on, the addEventListener will execute the writePassword function in response. 
-
-// (3)a function titled writePassword, which will execute whenever the generateBtn gets clicked, will perform a few functions.
-// Before we talk about those functions, we first have to identify a couple variables' values.
-// the first one is a variable titled password. password's value is equal to the function titled generatePassword.
-// !!!!! // generatePassword is a function we have to write in this homework!!
-// the next variable is called password.Text, and it's value is equal to the location of the html element with the id #password (the text area where we will view the randomly generated password)
-// !!!!! // do we need to add an element to the DOM or is that module 4? I think that's module 4...
-// the value of passwordText would be the content written in the text area, in this case the generate password. We know this is true because that value is literally equal to the variable titled password.
-// !!!!! // don't forget that the variable titled password is also equal to the function named generatePassword - a chunk of code that we want to write, that will be a function and when it's performing, the code for generating a random password will occur.
